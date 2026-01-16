0936df48fb1b7efc947b35c864f9de8078e3267b
ahmedadeyemi-cts <ahmedadeyemi@gmail.com>
2026-01-16 14:36:32 -0800

    Update history.js

--- functions/api/admin/oncall/history.js
+++ functions/api/admin/oncall/history.js
@@ -1,3 +1,4 @@
+//functions/api/admin/oncall/history.js
 export async function onRequest({ request, env }) {
   const jwt = request.headers.get("cf-access-jwt-assertion");
   if (!jwt) return new Response("Unauthorized", { status: 401 });
